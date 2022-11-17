7da237e99ddb9865dcf2372e5de6a8ff2da0cdf5
Gustom49 <115970395+Gustom49@users.noreply.github.com>
2022-11-17 16:06:52 +1000

    Update Text based adventure game.py

--- Text based adventure game.py
+++ Text based adventure game.py
@@ -1,48 +1,74 @@
-# dictionary
+from time import sleep
+from threading import *
+
+from re import T
+ #dictionary
 
 rooms = {'start': 
          {'name': 'the front porch',
           'north': 'lounge room', 
           'contents': [],
-        'text': 'your standing at the front porch./ntheres a wooden door in frot of you.'},
+        'text': 'your standing at the front porch.theres a wooden door in frot of you.\n derections were you can go: north:'},
     
          'lounge room': 
          {'name': 'the lounge room',
-          'east': '1st pt of hallway', 'south': 'start', 
-        'text': 'you are in the lounge/kichen. there a tv,/n dinning room table. and the couch', 
+          'east': '1st pt of hallway', 
+          'south': 'start', 
+          'north': 'kichen',
+        'text': 'you are in the lounge. there a tv,\n dinning room table. and the couch\n derections were you can go: east, south, north:', 
         'contents':  ['couch', 'tv', 'dinning room table', 'rug']},
+
+        'kichen': 
+         {'name': 'the kichen',
+          'north': 'cuboard',
+           'south': 'lounge', 
+        'text': 'you are in the kichen. there be food, a\n derections were you can go: north, south:', 
+        'contents':  ['frig', 'oven', 'dishwasher', 'ketal', 'sink']},
          
+         'cuboard': 
+         {'name': 'the cuboard',
+           'east': 'vegimit shapes',
+           'south': 'kichen', 
+        'text': 'you are in the cubord. there be food, a\n derections were you can go: east, south:', 
+        'contents':  ['smith chips', 'pasta', 'dog food', 'biskit', 'tolet paper']},
+         
+         'vegimit shapes': 
+         {'name': 'vegimit shapes',
+           'west': 'cuboard', 
+        'text': 'oven baked, not fried \n derections were you can go: west', 
+        'contents':  ['flavour you can see',  'tasmanis shape' , 'australia shape']},
+    
     '1st pt of hallway':
          {'name': 'the hall way',
           'west': 'lounge room',
           'south': 'bedroom', 
           'east': '2nd pt of hallway',
           'contents': ['hall way rug', 'picher frames'],
-      'text': 'a very very suspisous hall way.'},
+      'text': 'a very very suspisous hall way.\n derections were you can go: west,south,east:'},
     
          'bedroom':
          {'name': 'the bedroom',
           'north': '1st pt of hallway', 
           'contents': ['sheets', 'bed', 'lego', 'tv'],
-        'text': 'This is a your bed room, its an avrege/nbedroom, no tolet in here.'},
+        'text': 'This is a your bed room, its an avrege\nbedroom, no tolet in here.\n derections were you can go: north:'},
         
         '2nd pt of hallway' :
          {'name': 'the 2nd pt of hallway', 
           'east': 'parents bedroom',
           'north': 'tolet', 
-        'text': 'you are in the 2nd pt of hallway. ', 
+        'text': 'you are in the 2nd pt of hallway. \n derections were you can go: north,east',
         'contents': ['picher frame', 'rug',]},  
   
-         'parents bedroom': 
+         'parents bedroom' :
          {'name' : 'the parents bed room', 
           'west' : '2nd pt of hallway', 
-        'text': 'you are in the parents bed room. ', 
+        'text': 'you are in the parents bed room. \nderections were you can go: west', 
         'contents': ['king sized beed', 'tv',]},   
    
          'tolet': 
          {'name' : 'the holy grail its self(the tolet)',
           "south" : '2nd pt of hallway', 
-        'text': 'the holy grail(tolet). ', 
+        'text': 'the holy grail(tolet).   \n derections were you can go: nowere else \n Please press enter to end game', 
         'contents': ['tolet', 'plunger',]}}    
            
 directions = ['north', 'south', 'east', 'west']
@@ -52,40 +78,31 @@ carrying = []
 
 print("             TOLET TOM")
 
-print("Youve gust got back from a long day at school,/nuve taking the bus home and arive at the front door.")
-print("you havent gone to the tolet all day,/nyouve been avoding them at school becouse there are the most discusting palce youve every seen.Theres tolet paper on the wallls, pee stans on the celings, and skid marks on the doors. You refuse to go there .but that means evry day after school its a rush to the tolet.")
+print("Youve gust got back from a long day at school,\nuve taking the bus home and arive at the front door.")
+print("you havent gone to the tolet all day,\nyouve been avoding them at school becouse there are the most discusting palce youve every seen.\nTheres tolet paper on the wallls, pee stans on the celings, and skid marks on the doors.\n You refuse to go there .but that means evry day after school its a rush to the tolet.")
 print("Your objective: Get to the TOLET in time TOM")
-
-
 
 while True:
     # this displays current location
     print()
     print('You are in {}.'.format(current_room['name']))
     print(current_room['text'])
-  
     # this displays objects
     if current_room['contents']:
         print('In the room are: {}'.format(', '.join(current_room['contents'])))
-   
     # gets input
     command = input('\nWhat do you do?(north,west,east,south): ').strip()
-  
-    # the player movement(north,west,east,south)
+    # the player movement(north,west,east
     if command in directions:
         if command in current_room:
             current_room = rooms[current_room[command]]
         else:
             # not very cool movement
             print("You can't go that way.")
-   
     # to Quit the game
     elif command.lower() in ('q', 'quit'):
         break
-
-#INVENTORY SYSTEM
-
- # gather objects
+    # gather objects
     elif command.lower().split()[0] == 'get':
         item = command.lower().split()[1]
         if item in current_room['contents']:
@@ -93,7 +110,6 @@ while True:
             carrying.append(item)
         else:
             print("I don't see that here.")
-   
     # get rid of objects
     elif command.lower().split()[0] == 'drop':
         item = command.lower().split()[1]
@@ -102,15 +118,15 @@ while True:
             carrying.remove(item)
         else:
             print("You aren't carrying that.")
-   
     # icky comands
     else:
         print("I don't understand that command.")
 
-       
-       
-        # this is the timer tom
-        import time
+   
+
+# this is the timer tom
+
+import time
   
 
 def countdown(t):
@@ -126,7 +142,7 @@ def countdown(t):
   
   
 # the time in seconds(2mins)
-t = int(10)
+t = int(120)
   
 # the  coundown
 countdown(int(t))
@@ -136,5 +152,7 @@ countdown(int(t))
     # get timer to say to the code to stop game
     # when th tolet is reached the gamenees to stop
    # add more room
+
+   
 
    
